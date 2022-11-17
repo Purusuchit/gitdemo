0d8374abb7041346c92e451d9a62f2b7c6268db9
Kajal-Shukla1 <kajalshukla124@gmail.com>
2022-11-17 20:08:59 +0530

    git demo

---
+++ app/src/main/java/com/myhbd/myhbd/birthdaygreetings.kt
@@ -0,0 +1,11 @@
+package com.myhbd.myhbd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class birthdaygreetings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthdaygreetings)
+    }
+}
